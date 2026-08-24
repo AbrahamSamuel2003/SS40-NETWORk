2ff2f2a9202017ebd8dfd15d6c937235f67bb911
Abraham Samuel <abrahamsamuel645@gmail.com>
2026-08-24 13:22:20 +0530

    feat: add high-resolution compressed Open Graph and Twitter card banner

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -47,11 +47,20 @@ export async function generateMetadata(): Promise<Metadata> {
       siteName: company,
       locale: "en_US",
       type: "website",
+      images: [
+        {
+          url: "/og-image.jpg",
+          width: 1200,
+          height: 630,
+          alt: "SS40 Network - Engineering Intelligent Digital Solutions, Building Innovative Products & Empowering Future Leaders",
+        },
+      ],
     },
     twitter: {
       card: "summary_large_image",
       title: defaultTitle,
       description: defaultDesc,
+      images: ["/og-image.jpg"],
     },
     robots: {
       index: true,
